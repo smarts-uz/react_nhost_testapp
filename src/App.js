3f2bdc2ffd33dfaf8bbadeed64c9127b46dcb70e
JasurbekErgashev <jergashev633@gmail.com>
2023-08-01 11:17:39 +0500

    integrated with nhost

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NhostClient, NhostProvider } from '@nhost/react'
 import { Toaster } from 'react-hot-toast';
 
 import Layout from './components/Layout';
@@ -7,9 +8,14 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
 
+const nhost = new NhostClient({
+  subdomain: process.env.REACT_APP_NHOST_SUBDOMAIN,
+  region: process.env.REACT_APP_NHOST_REGION
+})
+
 function App() {
   return (
-    <>
+    <NhostProvider nhost={nhost}>
       <BrowserRouter>
         <Routes>
           <Route path="sign-up" element={<SignUp />} />
@@ -22,7 +28,7 @@ function App() {
       </BrowserRouter>
 
       <Toaster />
-    </>
+    </NhostProvider>
   );
 }
 
